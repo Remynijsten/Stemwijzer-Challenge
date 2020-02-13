490e14d8174a3e6ed561e7a84978b629a698da26
Remy Nijsten <99012252@mydavinci.nl>
2020-02-13 11:14:03 +0100

    Updated Opinion Seed
    Added position seperation

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const startBack = document.getElementsByClassName("startBack")[0];
 const poll = document.getElementsByClassName("poll")[0];
 const progress = document.getElementsByClassName("progress")[0];
 
+var questionIndex = 0;
+
 start.onclick = function(){
 	startBack.style.display = "none";
 	poll.style.display = "block";
-	statement.innerHTML = "1. " + subjects[0]["title"];
-	comment.innerHTML = subjects[0]["statement"];
-	seedOpinion(0);
+	seedStatement(questionIndex);
 }
 
 console.log(subjects);
@@ -19,11 +19,16 @@ function toggleModal(config){
 }
 
 function seedOpinion(index){
+	for(i=0;i<23;i++){
+		var appendParent;
 
-	// partijen die het eens zijn
-	for(i=0;i<14;i++){
+		subjects[index]["parties"][i]["position"] == "pro" ? appendParent = 0 : "";
+		subjects[index]["parties"][i]["position"] == "contra" ? appendParent = 2 : "";
+		subjects[index]["parties"][i]["position"] == "none" ? appendParent = 1 : "";
+		
 		var wrapper = document.createElement("div");
 		wrapper.classList.add('w3-container', 'opinionRow');
+		wrapper.id = "opinionRow";
 
 		var btn = document.createElement("button");
 		btn.setAttribute("onclick", "openAccordion(this.innerHTML)");
@@ -40,60 +45,24 @@ function seedOpinion(index){
 		opinion.innerHTML = subjects[index]["parties"][i]["opinion"];
 		opinion.classList.add('w3-hide', 'w3-container', 'w3-animate-opacity');
 		wrapper.appendChild(opinion);
-		document.getElementsByClassName("partyOpinion")[0].appendChild(wrapper);
-	}
-	
-	// Niet stemmers
-	var wrapper = document.createElement("div");
-	wrapper.classList.add('w3-container', 'opinionRow');
-
-	var btn = document.createElement("button");
-	btn.setAttribute("onclick", "openAccordion(this.innerHTML)");
-	btn.classList.add('w3-button', 'w3-block');
-	btn.style.borderBottom = "1px solid black";
-	btn.style.background = "none";
-	btn.style.color = "black";
-	btn.style.textAlign = "left";
-	btn.innerHTML = subjects[index]["parties"][14]["name"];
-	wrapper.appendChild(btn);
-
-	var opinion = document.createElement("div");
-	opinion.setAttribute("id", subjects[index]["parties"][14]["name"]);
-	opinion.innerHTML = subjects[index]["parties"][14]["opinion"];
-	opinion.classList.add('w3-hide', 'w3-container', 'w3-animate-opacity');
-	wrapper.appendChild(opinion);
-	document.getElementsByClassName("partyOpinion")[1].appendChild(wrapper);
-
-	// Partijen die het oneens zijn
-	for(i=15;i<23;i++){
-		var wrapper = document.createElement("div");
-		wrapper.classList.add('w3-container', 'opinionRow');
-
-		var btn = document.createElement("button");
-		btn.setAttribute("onclick", "openAccordion(this.innerHTML)");
-		btn.classList.add('w3-button', 'w3-block');
-		btn.style.borderBottom = "1px solid black";
-		btn.style.background = "none";
-		btn.style.color = "black";
-		btn.style.textAlign = "left";
-		btn.innerHTML = subjects[index]["parties"][i]["name"];
-		wrapper.appendChild(btn);
-
-		var opinion = document.createElement("div");
-		opinion.setAttribute("id", subjects[index]["parties"][i]["name"]);
-		opinion.innerHTML = subjects[index]["parties"][i]["opinion"];
-		opinion.classList.add('w3-hide', 'w3-container', 'w3-animate-opacity');
-		wrapper.appendChild(opinion);
-		document.getElementsByClassName("partyOpinion")[2].appendChild(wrapper);
+		document.getElementsByClassName("partyOpinion")[appendParent].appendChild(wrapper);
 	}
 }
 
+function seedStatement(index){
+	statement.innerHTML = "1. " + subjects[index]["title"];
+	comment.innerHTML = subjects[index]["statement"];
 
-
-
-
-
-
+	for(i=0;i<3;i++){
+		var parent = document.getElementsByClassName("partyOpinion")[i];
+		var childs = document.getElementsByClassName("opinionRow");
+		for (x=0;x<childs.length;x++){
+			childs[x].parentNode.removeChild(childs[x]);
+		}
+	}
+	
+	seedOpinion(index);
+}
 
 function openAccordion(party){
 	var x = document.getElementById(party);
